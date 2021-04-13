03857a58d0fc439b2902b9932124be28e336e845
flipxonsoft <manishjobs1999@gmail.com>
2021-04-13 14:04:58 +0530

    [Homework_1] immutable staring

---
+++ javascript/Homework_1/immutableString.js
@@ -0,0 +1,8 @@
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+// for(let i=1;i<myStr.length;i++)
+myStr[0] = "H"
+console.log(myStr) // Change this line
+// Only change code above this line
