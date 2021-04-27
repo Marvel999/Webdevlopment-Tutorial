f2982c0b620bae5b8fe885a532ca99fee79345e4
Marvel999 <manishjobs1999@gmail.com>
2021-04-27 18:57:57 +0530

    node module

--- javascript/javaScript_6/htmlRead.js
+++ javascript/javaScript_6/htmlRead.js
@@ -1,7 +1,12 @@
 const fs=require("fs");
+const cheerio=require("cheerio");
 
 let htmlKaData=fs.readFileSync("./index.html");
-console.log(htmlKaData+"");
+// console.log(htmlKaData+"");
 // file -> htmlFile -> Stringified
 
 // npm-> node package manager
+let ch=cheerio.load(htmlKaData);
+let h1Element=ch("h1").text();
+
+console.log(h1Element)
